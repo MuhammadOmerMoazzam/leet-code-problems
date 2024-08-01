a59934f16d3d8813dae69896ddc99212906b3592
Muhammad Omer Moazzam <m.omermoazzam04@gmail.com>
2024-08-01 18:44:08 +0500

    Time: 0 ms (100.00%) | Memory: 41.1 MB (91.64%) - LeetSync

---
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        
+        int index;
+
+        if (haystack.contains(needle)){
+
+            return index =  haystack.indexOf(needle);
+
+        } return index = -1;
+        
+        
+    }
+}
