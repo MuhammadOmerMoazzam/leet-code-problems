9bc3670285c7957d14beee873fd6d4f64ec11c48
Muhammad Omer Moazzam <m.omermoazzam04@gmail.com>
2024-11-29 17:41:22 +0500

    Time: 0 ms (100.00%) | Memory: 45.8 MB (25.30%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,15 +1,17 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        
-        for (int i=0; i<nums.length; i++) {
-
-            if (nums[i] == target) {
-
-                return i;       
-
+    public int search(int[] arr, int value) {
+        int low = 0, high = arr.length-1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            
+            if (value == arr[mid])
+                return mid;
+            if (value > arr[mid])
+                low = mid + 1;
+            else {
+                high = mid - 1;
             }
-        } 
-        
+        }
         return -1;
     }
 }
